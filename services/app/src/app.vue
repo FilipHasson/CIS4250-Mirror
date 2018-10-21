4a4c9db24e23cc8fd837ec252e1ec429c6d4bdf2
Jessy Williams <jessy@jessywilliams.com>
2018-10-21 19:51:16 -0400

    Moves general style config into app.vue

--- services/app/src/app.vue
+++ services/app/src/app.vue
@@ -1,10 +1,3 @@
-<template>
-  <div id="app">
-    <NavigationHeader />
-    <router-view></router-view>
-  </div>
-</template>
-
 <script>
 import NavigationHeader from '@/components/navigation/header'
 export default {
@@ -15,5 +8,41 @@ export default {
 }
 </script>
 
+<template>
+  <div id="app">
+    <NavigationHeader />
+    <router-view />
+  </div>
+</template>
+
 <style lang="scss">
+html {
+  font-size: 18px;
+}
+
+body {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  font-family: sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  font-size: 1.25rem;
+  margin-bottom: 1rem;
+  margin-top: 2rem;
+}
+
+h2 {
+  font-size: 1.15rem;
+  margin-bottom: 1rem;
+}
+
+.page {
+  margin: 1rem;
+}
 </style>
