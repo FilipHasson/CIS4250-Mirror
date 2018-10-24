45516db10be113f2d1239d8f3b6290a0cddc9ee6
Jessy Williams <jessy@jessywilliams.com>
2018-10-24 16:42:36 -0400

    Adds account and recipe vuex mutations and actions

---
+++ services/app/src/utils/store.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import actions from '@/utils/actions'
+import getters from '@/utils/getters'
+import mutations from '@/utils/mutations'
+import state from '@/utils/state'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: true,
+  actions,
+  getters,
+  mutations,
+  state
+})
